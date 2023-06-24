b196edf34ab67b69cd33f26e5019486a56e322c6
scobca <scobca18@yandex.ru>
2023-06-25 01:40:01 +0300

    JavaRush module 1 - base

--- app/src/main/java/try4/app/App.java
+++ app/src/main/java/try4/app/App.java
@@ -3,14 +3,22 @@
  */
 package try4.app;
 
-import org.apache.commons.text.WordUtils;
-import try4.utilities.Printer;
+//import org.apache.commons.text.WordUtils;
+//import try4.utilities.Printer;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+//    public static void main(String[] args) throws Exception {
+//
+//        Printer printer = new Printer("Print this");
+//        Printer.text = "testste";
+//        Printer.print();
+//    }
 
-        Printer printer = new Printer("Print this");
-        Printer.text = "testste";
-        Printer.print();
+    public static void main(String[] args) {
+        int a = 120;
+        String b = "Strings";
+
+        //в модуле 1 чекаем комменты, чисто база
+        System.out.print(a + " " +b);
     }
 }
